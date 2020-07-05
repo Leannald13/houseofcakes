cc0a105ad2e20217990df642ace85adc8aa591b1
Leannald13 <leannald13@gmail.com>
2020-07-05 16:32:34 +0000

    created product template and view and url

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'image',
+        'price',
+        'name',
+    )
 
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
