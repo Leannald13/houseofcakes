5321ea56da8216c05143da604011eb4434dcebaa
Leannald13 <leannald13@gmail.com>
2020-07-13 18:49:41 +0000

    created product detail view, url and template

--- products/views.py
+++ products/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 
 def all_products(request):
-    """A view to show all baked goods"""
+    """A view to show each product item and description"""
     products = Product.objects.all()
 
     context = {
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
