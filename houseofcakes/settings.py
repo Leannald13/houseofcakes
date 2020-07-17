bfec34757310f27d46eaef2653b2dc8531119029
Leannald13 <leannald13@gmail.com>
2020-07-17 19:49:40 +0000

    created contexts.py and added shopping bad functionality and delivery threshold

--- houseofcakes/settings.py
+++ houseofcakes/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth do not remove
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -149,3 +150,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
