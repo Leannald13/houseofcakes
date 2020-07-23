6ecc70f7076c283ff7eae28c2e4343331ad51321
Leannald13 <leannald13@gmail.com>
2020-07-23 17:11:41 +0000

    added crispy forms to settings.py and updated requirement.txt

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('date', 'delivery_cost', 'order_total', 'grand_total')
+
+
+admin.site.register(Order, OrderAdmin)
+
+
