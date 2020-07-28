6abb3d7b063849eb75b09ffcf001ad21e9793aa8
Leannald13 <leannald13@gmail.com>
2020-07-28 14:39:01 +0000

    initiating stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H9saXHQmyytvvuzZdiUPu88c1Sh8Ljhb0TmTeDRbQJvK1YNZ6WjsGboDxAGaTx9xTlCJrNeNY34eq8J2RnoYicV00wfRXpJkx',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
