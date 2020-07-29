1afc6d808c02ece26a52e428aba329b16a6981d6
Leannald13 <leannald13@gmail.com>
2020-07-29 11:45:26 +0000

    secret and public key variables set and intent added to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -19,13 +19,25 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51H9saXHQmyytvvuzZdiUPu88c1Sh8Ljhb0TmTeDRbQJvK1YNZ6WjsGboDxAGaTx9xTlCJrNeNY34eq8J2RnoYicV00wfRXpJkx',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
