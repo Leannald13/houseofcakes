b9245d3d6161a2fb92fbaad5544d3fd940f03620
Leannald13 <leannald13@gmail.com>
2020-08-03 16:18:18 +0000

    created aws account, s3 bucket, iam group, policy and user. Added secret keys to enenviron and updated settings.py

--- houseofcakes/settings.py
+++ houseofcakes/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'contact',
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -174,6 +175,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'houseofcakes'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get(AWS_ACCESS_KEY_ID)
+    AWS_SECRET_ACCESS_KEY_ID = os.environ.get(AWS_SECRET_ACCESS_KEY_ID) 
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
 
 # stripe
 FREE_DELIVERY_THRESHOLD = 50
