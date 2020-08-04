855eb8978aa45da7b94cdf943834c0d6a1127484
Leannald13 <leannald13@gmail.com>
2020-08-04 09:06:44 +0000

    disabled collectstatic=1

--- houseofcakes/settings.py
+++ houseofcakes/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# if os.path.exists("env.py"):
-#     import env
-import env
+if os.path.exists("env.py"):
+    import env
+
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
