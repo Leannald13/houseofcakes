3df0940e1105aad378d0ca3f7db50b5d4ea9f7b7
Leannald13 <leannald13@gmail.com>
2020-08-04 09:26:31 +0000

    removed second env2.py file

--- houseofcakes/env2.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = "2$b8p9mmed6g21o06n+6c4da6=^b3fm8o73o*n@n&$5t)zd0_y"
-
-os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_51H9saXHQmyytvvuzZdiUPu88c1Sh8Ljhb0TmTeDRbQJvK1YNZ6WjsGboDxAGaTx9xTlCJrNeNY34eq8J2RnoYicV00wfRXpJkx")
-os.environ.setdefault("STRIPE_SECRET", "sk_test_51H9saXHQmyytvvuzcLA2q879EaMAiXTKZoR50j5YlSeb7iXppMhpu9ujAtAWlMdzIXjhUW7N4xYtUvFyHdYX6IyP00h1N2InJe")
--- houseofcakes/settings.py
+++ houseofcakes/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['houseofcakes.herokuapp.com', 'localhost']
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [        
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
